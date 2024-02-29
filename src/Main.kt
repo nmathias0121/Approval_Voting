fde68d7d740f51f37c2601fe479f396be994a431
Neil Mathias <neilmathias25@gmail.com>
2024-02-29 15:25:46 +0530

    add small ballot file, add user input and import text file

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    println("Hello World!")
+    // input ballot text file location
+    // accepts both absolute path and repository path
+    // right click on text file to obtain/copy path
+    println("Enter the location of the ballot file?")
+    val ballotFileName = readLine()
+
+    // import ballot text file
+    val ballotFile = ballotFileName?.let { fileName ->
+        java.io.File(fileName).bufferedReader()
+    }
 }
