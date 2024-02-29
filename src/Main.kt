f8fa232bc6e8d392482192b28a30c455b6bd7abf
Neil Mathias <neilmathias25@gmail.com>
2024-02-29 22:47:00 +0530

    extract votes from ballot file

--- src/Main.kt
+++ src/Main.kt
@@ -9,4 +9,26 @@ fun main() {
     val ballotFile = ballotFileName?.let { fileName ->
         java.io.File(fileName).bufferedReader()
     }
+
+    val votes = mutableMapOf<String, Int>()
+    var numBallots = 0
+    var empty = 0
+    var full = 0
+
+    // extract votes out of ballot text file
+    var candidate = ballotFile?.readLine()
+    while (candidate != null) {
+        if ("none" in candidate) {
+            empty++
+        } else {
+            candidate.split(" ").distinct().forEach { vote ->
+                votes[vote] = votes.getOrDefault(vote, 0) + 1
+            }
+        }
+        if (ballotFile != null) {
+            candidate = ballotFile.readLine()
+        }
+        numBallots++
+    }
+    println(numBallots)
 }
